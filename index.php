84a82f69b108008daa82c6febab2043649d8ba01
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-15 11:37:22 +0100

    exo6

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $name = "Rico";
+    echo "Bonjour $name , comment vas tu ?";
+    ?>
+  </body>
